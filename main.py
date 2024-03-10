b575dc3e16a588ae6dcf483dfb1cadcfc160cbf0
Broadster <80648000+BroadsterYT@users.noreply.github.com>
2024-03-10 00:08:13 -0500

    New framerate system

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 """
 Main program. Contains the game loop.
 """
+import asyncio
 import os
 import sys
+import time
 
 import pygame
 from pygame.locals import QUIT
@@ -46,7 +48,7 @@ def redraw_game_window() -> None:
 main_room = rooms.Room(0, 0)
 
 
-def check_key_release(is_mouse) -> None:
+def check_key_release(event, is_mouse) -> None:
     """Checks if any input(s) has been released. If one has, then its count in key_released will be updated to match.
 
     Args:
@@ -68,44 +70,82 @@ def check_key_release(is_mouse) -> None:
 # ============================================================================ #
 #                                   Main Loop                                  #
 # ============================================================================ #
-def main():
+async def main(max_frame_rate):
     """Main sequence"""
+    loop = asyncio.get_event_loop()
+    next_frame_target = 0.0
+    sec_per_frame = 1 / max_frame_rate
+
     running = True
     while running:
+        if sec_per_frame:
+            # Frame rate limiter
+            delay = next_frame_target - time.time()
+            if delay > 0:
+                await asyncio.sleep(delay)
+            next_frame_target = time.time() + sec_per_frame
+
         for a_player in groups.all_players:
             if a_player.hp <= 0:
                 pygame.quit()
                 sys.exit()
 
-        global event  # noqa
-        for event in pygame.event.get():
-            key_pressed = pygame.key.get_pressed()
-
-            if event.type == QUIT or key_pressed[ctrl.K_PAUSE]:
-                sys.exit()
-
-            # Key input updating
-            for key in ctrl.is_input_held.keys():
-                if key in [1, 2, 3]:
-                    ctrl.is_input_held[key] = pygame.mouse.get_pressed(5)[key - 1]
-                else:
-                    ctrl.is_input_held[key] = key_pressed[key]
-
-            # Key release updating
-            check_key_release(False)
-            check_key_release(True)
-
-            if event.type == pygame.MOUSEWHEEL:  # TODO: Implement this in players.py
-                # Player ammo refill
-                if main_room.player1.ammo < main_room.player1.max_ammo:
-                    main_room.player1.ammo += 1
+        # global event  # noqa
+        # for event in pygame.event.get():
+            # key_pressed = pygame.key.get_pressed()
+            #
+            # if event.type == QUIT or key_pressed[ctrl.K_PAUSE]:
+            #     sys.exit()
+            #
+            # # Key input updating
+            # for key in ctrl.is_input_held.keys():
+            #     if key in [1, 2, 3]:
+            #         ctrl.is_input_held[key] = pygame.mouse.get_pressed(5)[key - 1]
+            #     else:
+            #         ctrl.is_input_held[key] = key_pressed[key]
+            #
+            # # Key release updating
+            # check_key_release(False)
+            # check_key_release(True)
+            #
+            # if event.type == pygame.MOUSEWHEEL:  # TODO: Implement this in players.py
+            #     # Player ammo refill
+            #     if main_room.player1.ammo < main_room.player1.max_ammo:
+            #         main_room.player1.ammo += 1
 
         # ------------------------------ Game Operation ------------------------------ #
 
         # ------------------------------- Redraw Window ------------------------------ #
         redraw_game_window()
-        clock.tick(cst.FPS)
 
+        events_to_handle = list(pygame.event.get())
+        events_handled = loop.create_task(handle_events(events_to_handle))
+        await loop.run_in_executor(None, pygame.display.flip)  # noqa
+        await events_handled
+
+
+async def handle_events(events_to_handle):
+    for any_event in events_to_handle:
+        key_pressed = pygame.key.get_pressed()
+
+        if any_event.type == QUIT or key_pressed[ctrl.K_PAUSE]:
+            sys.exit()
+
+        # Key input updating
+        for key in ctrl.is_input_held.keys():
+            if key in [1, 2, 3]:
+                ctrl.is_input_held[key] = pygame.mouse.get_pressed(5)[key - 1]
+            else:
+                ctrl.is_input_held[key] = key_pressed[key]
+
+        # Key release updating
+        check_key_release(any_event, False)
+        check_key_release(any_event, True)
+
+        if any_event.type == pygame.MOUSEWHEEL:  # TODO: Implement this in players.py
+            # Player ammo refill
+            if main_room.player1.ammo < main_room.player1.max_ammo:
+                main_room.player1.ammo += 1
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main(cst.FPS))
