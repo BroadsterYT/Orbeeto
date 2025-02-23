8de10b3400ca0b38ec754d89217f42d6e7e7a2f2
BroadsterYT <80648000+BroadsterYT@users.noreply.github.com>
2025-02-22 19:26:15 -0500

    Decent camera panning functionality

--- Orbeeto/Camera.cpp
+++ Orbeeto/Camera.cpp
@@ -2,6 +2,7 @@
 #include "Components/Transform.hpp"
 #include "WindowManager.hpp"
 
+#include <algorithm>
 #include <cmath>
 #include "SDL_stdinc.h"
 #include "Math.hpp"
@@ -48,20 +49,18 @@ void Camera::focus(int posX, int posY) {
 }
 
 void Camera::cinematicFocus(int posX, int posY) {
-	if (camera.x < posX - WindowManager::SCREENWIDTH / 2) {
-		accel.x = accelConst;
-	}
-	if (camera.x > posX - WindowManager::SCREENWIDTH / 2) {
-		accel.x = -accelConst;
-	}
+	double tanFuncX = tan(M_PI / (2.0 * (double)WindowManager::SCREENWIDTH) * ((double)camera.x - (double)posX + (double)WindowManager::SCREENWIDTH) - M_PI / 4.0);
+	double tanFuncY = tan(M_PI / (2.0 * (double)WindowManager::SCREENHEIGHT) * ((double)camera.y - (double)posY + (double)WindowManager::SCREENHEIGHT) - M_PI / 4.0);
 
-	if (camera.y < posY - WindowManager::SCREENHEIGHT / 2) {
-		accel.y = accelConst;
-	}
-	if (camera.y > posY - WindowManager::SCREENHEIGHT / 2) {
-		accel.y = -accelConst;
-	}
+	double mult = 6.0;
+	double tempAccelX = std::clamp(-mult * pow(tanFuncX, 3.0), -10.0, 10.0);
+	double tempAccelY = std::clamp(-mult * pow(tanFuncY, 3.0), -10.0, 10.0);
+
+	std::cout << tempAccelY << std::endl;
 	
+	accel.x = tempAccelX;
+	accel.y = tempAccelY;
+
 	accel.x += vel.x * fric;
 	accel.y += vel.y * fric;
 	vel += accel * TimeManip::getDeltaAdjuster();
